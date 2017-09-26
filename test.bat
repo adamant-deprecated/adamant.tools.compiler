800534c0c649074db010a2e3251db6edd0c2c967
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2017-09-26 00:19:03 -0400

    update bat file to test translation

--- test.bat
+++ test.bat
@@ -1,5 +1,7 @@
 REM This builds the compiler and runs it on the test-cases to test compiler output
 CALL build.bat
+rmdir /S/Q translated\test-cases
+mkdir translated\test-cases
 for %%E in (test-cases\*.ad) do (
-	target\Program.exe %%E translated\text-cases\%%~nE
+	target\current\Program.exe %%E translated\test-cases\%%~nE.cpp
 )
