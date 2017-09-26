0cfe7dbdbd60f7d68590ec4b4c4e520e77ed8f78
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2017-09-26 00:11:55 -0400

    add TODOs, change var to let some places, change code quotes to backtick

--- buildSelf.bat
+++ buildSelf.bat
@@ -1,5 +1,5 @@
 REM this builds the current version, then uses it on itself
 CALL build.bat
-target\current\Program.exe src\Program.ad translated\current
+target\current\Program.exe src\Program.ad translated\current\Program.cpp
 xcopy src\runtime.cpp translated\current /Y
 clang translated\current\Program.cpp -o target\current\Program.exe
