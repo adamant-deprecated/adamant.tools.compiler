e3dc56a98d5e0678e6c1ed6d19dbde6bca75cd06
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2017-09-29 19:01:04 -0400

    set up more implementation.  Try to implement basic enums, but accessing enum values doesn't work

---
+++ test-cases/EnumStruct.cpp
@@ -0,0 +1,29 @@
+#include "runtime.h"
+
+// Declarations
+enum day_of_week;
+auto Main() -> void;
+
+// Class Declarations
+enum day_of_week
+{
+	Sunday = 0,
+	Monday = 1,
+	Tuesday,
+	Wednesday,
+	Thursday,
+	Friday,
+	Saturday,
+};
+
+// Definitions
+auto Main() -> void
+{
+}
+
+// Entry Point Adapter
+int main(int argc, char const *const * argv)
+{
+	Main();
+	return 0;
+}
