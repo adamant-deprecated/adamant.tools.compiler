bd3ce34a2df04bd9c53d82124eee3657bfe8077e
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2017-12-30 23:45:00 -0500

    adding stuff for error reporting #35

---
+++ test-cases/ints/Precedence.cpp
@@ -0,0 +1,26 @@
+#include "RuntimeLibrary.h"
+
+// Type Declarations
+
+// Function Declarations
+auto Main_() -> void;
+
+// Class Declarations
+
+// Global Definitions
+
+// Definitions
+
+auto Main_() -> void
+{
+	p_int x_;
+	x_ = p_int(1)->op_Add(p_int(3)->op_Subtract(p_int(1))->op_Divide(p_int(2)));
+	x_ = p_int(1)->op_Add(p_int(3))->op_Subtract(p_int(1)->op_Divide(p_int(2)));
+}
+
+// Entry Point Adapter
+std::int32_t main(int argc, char const *const * argv)
+{
+	Main_();
+	return 0;
+}
