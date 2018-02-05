6675e291976a7603f7808d73ef37f0753447ff22
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-02-04 21:43:10 -0500

    add basic struct support as first step in properly distinguishing reference and value types #27

---
+++ test-cases/structs/Declaration.cpp
@@ -0,0 +1,53 @@
+#include "RuntimeLibrary.h"
+
+// Type Declarations
+struct test_;
+
+// Function Declarations
+auto Main_() -> void;
+
+// Class Declarations
+
+struct test_
+{
+public:
+	p_string Name_;
+	test_(p_string const name_);
+	auto Method_() const -> p_string;
+};
+
+// Global Definitions
+
+// Definitions
+
+::test_::test_(p_string const name_)
+{
+	Name_ = name_;
+}
+
+auto ::test_::Method_() const -> p_string
+{
+	return Name_;
+}
+
+auto Main_() -> void
+{
+	::test_ const t_ = ::test_(p_string("Bob"));
+	t_->Method_();
+}
+
+// Entry Point Adapter
+std::int32_t main(int argc, char const *const * argv)
+{
+	try
+	{
+		Main_();
+		return 0;
+	}
+	catch(std::exception &ex)
+	{
+		std::printf("Program exited due to error:\n");
+		std::printf("%s", ex.what());
+		return 70;
+	}
+}
