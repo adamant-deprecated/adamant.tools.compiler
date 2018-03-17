1ee33bbc3158164774de42ce2e038f5932372661
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-03-17 00:10:20 -0400

    test if multiple variables with the same name are distinguished for type purposes

---
+++ test-cases/types/mutlipleVariblesWithSameName.cpp
@@ -0,0 +1,58 @@
+#include "RuntimeLibrary.h"
+
+// Type Declarations
+class Test_;
+
+// Function Declarations
+auto Main_() -> p_int;
+
+// Class Declarations
+
+class Test_
+{
+public:
+	p_bool op_Equal(Test_ const * other) const { return this == other; }
+	p_bool op_NotEqual(Test_ const * other) const { return this != other; }
+	p_int value_;
+	auto construct() -> ::Test_*;
+};
+
+// Global Definitions
+
+// Definitions
+
+auto ::Test_::construct() -> ::Test_*
+{
+	::Test_* self = this;
+	value_ = p_int(0);
+	return self;
+}
+
+auto Main_() -> p_int
+{
+	if (p_bool(true).Value)
+	{
+		p_int const t_ = p_int(0);
+		return t_;
+	}
+	else
+	{
+		::Test_ const *_Nonnull const t_ = (new ::Test_())->construct();
+		return t_->value_;
+	}
+}
+
+// Entry Point Adapter
+std::int32_t main(int argc, char const *const * argv)
+{
+	try
+	{
+		return Main_().Value;
+	}
+	catch(std::exception &ex)
+	{
+		std::printf("Program exited due to error:\n");
+		std::printf("%s", ex.what());
+		return 70;
+	}
+}
