372c70b8af5def109f1508fde8306d8a84e0fd43
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-03-18 17:19:25 -0400

    support checking list equality, change primitives package name

---
+++ test-cases/lists/equality.cpp
@@ -0,0 +1,35 @@
+#include "RuntimeLibrary.h"
+
+// Type Declarations
+
+// Function Declarations
+auto Main_() -> void;
+
+// Class Declarations
+
+// Global Definitions
+
+// Definitions
+
+auto Main_() -> void
+{
+	::System_::Collections_::List_<p_int> const *_Nonnull const values_ = (new ::System_::Collections_::List_<p_int>())->construct();
+	values_->op_Equal(values_);
+	values_->op_NotEqual(values_);
+}
+
+// Entry Point Adapter
+std::int32_t main(int argc, char const *const * argv)
+{
+	try
+	{
+		Main_();
+		return 0;
+	}
+	catch(std::exception &ex)
+	{
+		std::printf("Program exited due to error:\n");
+		std::printf("%s", ex.what());
+		return 70;
+	}
+}
