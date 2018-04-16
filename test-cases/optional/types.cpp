255f0710ccfd374656c5fbc4dbb0a9297f8bf4bf
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-04-16 09:22:07 -0400

    fix more emitting of optional types

--- test-cases/optional/types.cpp
+++ test-cases/optional/types.cpp
@@ -4,7 +4,7 @@
 class Test_;
 
 // Function Declarations
-auto Func_(p_optional<p_string> const x_, p_optional<p_uint> const y_) -> p_optional<p_int> const;
+auto Func_(p_optional<p_string> const x_, p_optional<p_uint> const y_) -> p_optional<p_int>;
 auto Main_(::System_::Console_::Console_ *_Nonnull const console_) -> void;
 
 // Class Declarations
@@ -21,14 +21,14 @@ public:
 
 // Definitions
 
-auto Func_(p_optional<p_string> const x_, p_optional<p_uint> const y_) -> p_optional<p_int> const
+auto Func_(p_optional<p_string> const x_, p_optional<p_uint> const y_) -> p_optional<p_int>
 {
 	p_optional<p_int> const z_ = p_none;
 	::Test_ const *_Nullable const t_ = p_none;
 	::System_::Text_::String_Builder_ *_Nullable const sb_ = p_none;
-	::System_::Collections_::List_<p_optional<p_int> const> const *_Nonnull const l_ = (new ::System_::Collections_::List_<p_optional<p_int> const>())->construct();
+	::System_::Collections_::List_<p_optional<p_int>> const *_Nonnull const l_ = (new ::System_::Collections_::List_<p_optional<p_int>>())->construct();
 	::Test_ *_Nullable const m_ = p_none;
-	p_optional<p_optional<p_int> const> const d_ = p_none;
+	p_optional<p_optional<p_int>> const d_ = p_none;
 	return p_none;
 }
 
