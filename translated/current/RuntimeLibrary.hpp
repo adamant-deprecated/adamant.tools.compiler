6aab811d1fc8a9e5e415d0205df7edbda46af671
AppVeyor Build <>
2018-05-02 07:01:28 -0700

    Translated files for a3ab860ca5d4cc812c70af3de705655301b27f44 'Merge pull request #68 from adamant/feature/emit-c
    Shift gears towards emitting C, some type handling'

--- translated/current/RuntimeLibrary.hpp
+++ translated/current/RuntimeLibrary.hpp
@@ -7,307 +7,36 @@
 #include <stdexcept>
 #include <string>
 
+// Double underscores replace dots
+// Runs of more than one underscore, add an underscore (except at end)
+// TODO what about functionst that end with underscores?
+// Functions suffix with double underscore then number of args (then types)
+// Types and variables
+// Pritives:
+// * code_point
+// * i32, u32
+// * size
+// * string
+// * optional__
+// # Prefixes
+// * c_ class
+// * s_ struct
+// * m_ method
+// * f_ function
+// # Combinable prefixes
+// * r reference
+// * rv refernce to variable
+// * o optional
+// * rv_Foo - reference to foo
+// # Generic Argucments
+// * c_Foo__3__i32_5s_Bar_string_
+int x__y();
+int m_x___y__0();
 
 // -----------------------------------------------------------------------------
 // Utility Types
 // -----------------------------------------------------------------------------
 // Utility types that help ensure proper use of C++
-
-// A base class for all structs that prevents the default special members
-struct Struct_Base
-{
-protected:
-	Struct_Base() noexcept {}
-	Struct_Base(Struct_Base const & other) = delete;
-};
-
-// A base class for all classes that prevents the default special members
-// and ensures a virutal destructor
-class Class_Base
-{
-protected:
-	Class_Base() noexcept {}
-	Class_Base(Class_Base const & other) = delete;
-public:
-	virtual ~Class_Base() noexcept = default;
-};
-
-template<typename T>
-struct Optional final: Struct_Base
-{
-private:
-	bool has_value;
-	union
-	{
-		char dummy;
-		T value;
-	};
-
-public:
-	Optional() : has_value(false) {}
-	Optional(T&& value) : has_value(true) { new (&value) T(std::move(value)); }
-
-	T & operator->()
-	{
-		if(!has_value) throw std::runtime_error("Access to empty Optional value");
-		return &value;
-	}
-	T const & operator->() const
-	{
-		if(!has_value) throw std::runtime_error("Access to empty Optional value");
-		return &value;
-	}
-	T & operator* ()
-	{
-		if(!has_value) throw std::runtime_error("Access to empty Optional value");
-		return &value;
-	}
-	T const & operator* () const
-	{
-		if(!has_value) throw std::runtime_error("Access to empty Optional value");
-		return &value;
-	}
-
-	~Optional()
-	{
-		if(has_value) (&value)->~T();
-	}
-};
-
-
-// -----------------------------------------------------------------------------
-// Runtime Types
-// -----------------------------------------------------------------------------
-// Types used to model the language semantics
-
-struct Borrows;
-
-/// Reference to the borrows tracker of a value
-struct Borrows_Ref final: Struct_Base
-{
-private:
-	Borrows *_Nullable borrows;
-
-	Borrows_Ref(Borrows *_Nullable borrows): borrows(borrows) {}
-	static auto own() -> Borrows_Ref { return Borrows_Ref(0); }
-
-public:
-	Borrows_Ref(Borrows_Ref&& other): borrows(other.borrows) {}
-
-	friend struct Borrows;
-};
-
-/// Tracks borrows at runtime
-struct Borrows final: Struct_Base
-{
-private:
-	const std::uint32_t Writing = UINT32_MAX;
-	std::uint32_t borrows = 0;
-
-public:
-	Borrows() noexcept {}
-	auto take_mut(char const *_Nonnull kind) -> Borrows_Ref;
-	auto take(char const *_Nonnull kind) -> Borrows_Ref;
-	auto borrow_mut(char const *_Nonnull kind) -> Borrows_Ref;
-	auto borrow(char const *_Nonnull kind) -> Borrows_Ref;
-	auto destruct(char const *_Nonnull kind) -> void;
-};
-
-template<typename T>
-struct ref_mut;
-
-template<typename T>
-struct ref;
-
-template<typename T>
-struct ref_own final: Struct_Base
-{
-private:
-	T *_Nonnull value;
-	Borrows mutable borrows;
-
-	// Must be constructed through alloc()
-	ref_own(T *_Nonnull other): value(other) {}
-
-public:
-	// no default constructor
-	ref_own() = delete;
-
-	// no copy constructor
-	ref_own(ref_own<T> const & other) = delete;
-
-	// default move constructor
-	ref_own(ref_own<T>&& other) noexcept = default;
-
-	// no copy assignment operator
-	ref_own<T>& operator=(ref_own<T> const& other) = delete;
-
-	// default move assignment operator
-	ref_own<T>& operator=(ref_own<T>&& other) noexcept = default;
-
-	// allocate a new owned object. NOT INITALIZED
-	// Note: this calls the default constructor because we have to setup the vtable
-	static auto alloc() -> ref_own<T> { return ref_own(new T()); }
-
-	auto take_mut() const -> ref_mut<T>
-	{
-		return ref_mut<T>(borrows.take_mut("owned reference"), value);
-	}
-	auto take() const -> ref<T>
-	{
-		return ref<T>(borrows.take("owned reference"), value);
-	}
-	auto borrow_mut() const -> ref_mut<T>
-	{
-		return ref_mut<T>(borrows.borrow_mut("owned reference"), value);
-	}
-	auto borrow() const -> ref<T>
-	{
-		return ref<T>(borrows.borrow("owned reference"), value);
-	}
-
-	~ref_own<T>()
-	{
-		borrows.destruct("owned reference");
-		delete value;
-	}
-};
-
-enum Binding_State
-{
-	Uninitialized,
-	Initialized,
-	Moved
-};
-
-template<typename T>
-struct Binding final: Struct_Base
-{
-private:
-	Optional<T> value;
-	Binding_State binding_state = Binding_State::Uninitialized;
-	Borrows borrows;
-	// Take the value, potentially leaving in a bad state
-	auto steal_value() -> T;
-
-public:
-	Binding() {}
-	Binding(T&& other): value(std::move(other)) {}
-	Binding(Binding<T>&& other): value(other.steal_value()) {}
-	// Move or copy (if implicitly copyable) value out of binding
-	auto move_value() -> T;
-};
-
-template<typename T>
-auto Binding<T>::move_value() -> T
-{
-	if(binding_state == Binding_State::Uninitialized)
-		throw std::runtime_error("Can't access a binding before it is initialized");
-	if(binding_state == Binding_State::Moved)
-		throw std::runtime_error("Can't access moved value");
-
-	if(T::ImplicitCopy)
-	{
-		return T::copy(*value);
-	}
-	else
-	{
-		binding_state = Binding_State::Moved;
-		return std::move(value);
-	}
-}
-
-template<typename T>
-struct var final: Struct_Base
-{
-public:
-	// default constructor
-	var() = default;
-
-	// var(T&& value): value(value) {}
-};
-
-template<typename T>
-struct let final: Struct_Base
-{
-private:
-	Binding<T> binding;
-public:
-	let() {};
-	explicit let(let<T>& other): binding(other.move_value()) {}
-	let(T&& other): binding(std::move(other)) {}
-	auto move_value() -> T { return binding.move_value(); }
-
-	// let(let<T>&& other): binding<T>((T)other) {}
-	// let(let<T>& other): binding<T>((T)other) {}
-	// no copy constructor
-	// let(let<T> const & other): base(other.value()) {}
-
-	// // default move constructor
-	// let(let<T>&& other) noexcept = default;
-
-	// // no copy assignment operator
-	// let<T>& operator=(let<T> const& other) = delete;
-
-	// // default move assignment operator
-	// let<T>& operator=(let<T>&& other) noexcept = default;
-
-	// This is the strange syntax for a move conversion operator
-	operator T() && { return binding.move_value(); }
-
-	// template<typename U>
-	// auto op_equal(U other) const -> decltype(binding<T const>::get_value().op_equal(other)) { return binding<T const>::get_value().op_equal(other); }
-};
-
-/// a mutable borrow
-template<typename T>
-struct ref_mut final: Struct_Base
-{
-private:
-	std::uint32_t mutable *_Nullable borrows;
-	T *_Nonnull value;
-	ref_mut(std::uint32_t *_Nullable borrows, T *_Nonnull value): borrows(borrows), value(value) {}
-
-public:
-	// no default constructor
-	ref_mut() = delete;
-
-	// no copy constructor
-	ref_mut(ref_mut<T> const & other) = delete;
-
-	// default move constructor
-	ref_mut(ref_mut<T>&& other) noexcept = default;
-
-	// no copy assignment operator
-	ref_mut<T>& operator=(ref_mut<T> const& other) = delete;
-
-	// default move assignment operator
-	ref_mut<T>& operator=(ref_mut<T>&& other) noexcept;
-
-	ref_mut(ref_own<T>&& other) { return other.take_mut(); }
-	ref_mut(ref_own<T>& other) { return other.borrow_mut(); }
-
-	friend struct ::ref_own<T>;
-};
-
-/// a non-mutable borrow
-template<typename T>
-struct ref: Struct_Base
-{
-private:
-	std::uint32_t mutable *_Nullable borrows;
-	T const *_Nonnull value;
-	ref(std::uint32_t *_Nullable borrows, T *_Nonnull value): borrows(borrows), value(value) {}
-
-public:
-	ref(ref_own<T>&& other) { return other.take(); }
-	ref(ref_own<T>& other) { return other.borrow(); }
-
-	template<typename U>
-	auto op_equal(U other) const -> decltype(value->op_equal(other)) { return value->op_equal(other); }
-
-	friend struct ::ref_own<T>;
-};
 
 // -----------------------------------------------------------------------------
 // Logical Operators
@@ -330,6 +59,30 @@ T op_or(T const & lhs, F rhs)
 // Primitive Types
 // -----------------------------------------------------------------------------
 
+struct p_bool
+{
+public:
+	// Runtime Members
+	bool value;
+
+	explicit p_bool() = default;
+	explicit p_bool(bool value): value(value) {}
+	p_bool& operator=(p_bool const& value) = default;
+
+	p_bool *_Nonnull operator->() { return this; }
+	p_bool const *_Nonnull operator->() const { return this; }
+	p_bool & operator* () { return *this; }
+	p_bool const & operator* () const { return *this; }
+
+	// Adamant Members
+	static auto construct() -> p_bool { return p_bool(false); }
+	p_bool op_not() const { return p_bool(!this->value); }
+	p_bool op_true() const { return p_bool(this->value); }
+	p_bool op_false() const { return p_bool(!this->value); }
+	p_bool op_and(p_bool other) const { return p_bool(this->value & other.value); }
+	p_bool op_or(p_bool other) const { return p_bool(this->value | other.value); }
+};
+
 class None
 {
 public:
@@ -339,7 +92,7 @@ public:
 static const None p_none = None();
 
 template<typename T>
-struct p_optional final: Struct_Base
+struct p_optional final
 {
 private:
 	bool hasValue;
@@ -349,8 +102,13 @@ private:
     };
 
 public:
+    // TODO make this constructor explicit
 	p_optional(T const & value) : data(value), hasValue(true) {}
+    // TODO make this constructor explicit
 	p_optional(None const none) : hasValue(false) {}
+    auto has_value() const -> p_bool { return p_bool(hasValue); }
+    auto value() const -> T { return data; }
+
 	T & operator->()
 	{
 		if(!hasValue) throw std::runtime_error("Access to `none` Optional value");
@@ -371,32 +129,6 @@ public:
 		if(!hasValue) throw std::runtime_error("Access to `none` Optional value");
 		return data;
 	}
-};
-
-struct p_bool
-{
-public:
-	// Runtime Members
-	bool value;
-
-	p_bool() = default;
-	p_bool(bool value): value(value) {}
-	p_bool& operator=(p_bool const& value) = default;
-
-	p_bool *_Nonnull operator->() { return this; }
-	p_bool const *_Nonnull operator->() const { return this; }
-	p_bool & operator* () { return *this; }
-	p_bool const & operator* () const { return *this; }
-
-	// Adamant Members
-	static auto construct() -> p_bool { return p_bool(false); }
-	p_bool op_not() const { return !this->value; }
-	p_bool op_true() const { return this->value; }
-	p_bool op_false() const { return !this->value; }
-	p_bool op_and(p_bool other) const { return this->value & other.value; }
-	p_bool op_or(p_bool other) const { return this->value | other.value; }
-	p_bool op_equal(p_bool other) const { return this->value == other.value; }
-	p_bool op_not_equal(p_bool other) const { return this->value != other.value; }
 };
 
 struct p_uint;
@@ -424,12 +156,10 @@ public:
 	static auto copy(p_int const & other) -> p_int { return other; }
 	void op_add_assign(p_int other) { this->value += other.value; }
 	void op_subtract_assign(p_int other) { this->value -= other.value; }
-	p_bool op_equal(p_int other) const { return this->value == other.value; }
-	p_bool op_not_equal(p_int other) const { return this->value != other.value; }
-	p_bool op_less_than(p_int other) const { return this->value < other.value; }
-	p_bool op_less_than_or_equal(p_int other) const { return this->value <= other.value; }
-	p_bool op_greater_than(p_int other) const { return this->value > other.value; }
-	p_bool op_greater_than_or_equal(p_int other) const { return this->value >= other.value; }
+	p_bool op_less_than(p_int other) const { return p_bool(this->value < other.value); }
+	p_bool op_less_than_or_equal(p_int other) const { return p_bool(this->value <= other.value); }
+	p_bool op_greater_than(p_int other) const { return p_bool(this->value > other.value); }
+	p_bool op_greater_than_or_equal(p_int other) const { return p_bool(this->value >= other.value); }
 	p_int op_add(p_int other) const { return this->value + other.value; }
 	p_int op_subtract(p_int other) const { return this->value - other.value; }
 	p_int op_negate() const { return -this->value; }
@@ -463,12 +193,10 @@ public:
 	static auto construct() -> p_uint { return 0; }
 	void op_add_assign(p_uint other) { this->value += other.value; }
 	void op_subtract_assign(p_uint other) { this->value -= other.value; }
-	p_bool op_equal(p_uint other) const { return this->value == other.value; }
-	p_bool op_not_equal(p_uint other) const { return this->value != other.value; }
-	p_bool op_less_than(p_uint other) const { return this->value < other.value; }
-	p_bool op_less_than_or_equal(p_uint other) const { return this->value <= other.value; }
-	p_bool op_greater_than(p_uint other) const { return this->value > other.value; }
-	p_bool op_greater_than_or_equal(p_uint other) const { return this->value >= other.value; }
+	p_bool op_less_than(p_uint other) const { return p_bool(this->value < other.value); }
+	p_bool op_less_than_or_equal(p_uint other) const { return p_bool(this->value <= other.value); }
+	p_bool op_greater_than(p_uint other) const { return p_bool(this->value > other.value); }
+	p_bool op_greater_than_or_equal(p_uint other) const { return p_bool(this->value >= other.value); }
 	p_uint op_add(p_uint other) const { return this->value + other.value; }
 	p_uint op_subtract(p_uint other) const { return this->value - other.value; }
 };
@@ -481,13 +209,14 @@ inline p_int::p_int(p_uint value)
 struct p_code_point
 {
 private:
-	std::uint32_t value;
+	std::uint32_t raw_value;
 
 public:
 	// Runtime Use Members
-	p_code_point() = default;
-	p_code_point(char value): value(value) {}
+	explicit p_code_point() = default;
+	explicit p_code_point(char value): raw_value(value) {}
 	char CharValue() const;
+    auto value() const -> std::uint32_t { return raw_value; }
 
 	p_code_point *_Nonnull operator->() { return this; }
 	p_code_point const *_Nonnull operator->() const { return this; }
@@ -495,14 +224,12 @@ public:
 	p_code_point const & operator* () const { return *this; }
 
 	// Adamant Members
-	static auto construct() -> p_code_point { return '\0'; }
-	p_bool op_equal(p_code_point const & other) const { return this->value == other.value; }
-	p_bool op_not_equal(p_code_point const & other) const { return this->value != other.value; }
+	static auto construct() -> p_code_point { return p_code_point('\0'); }
 	// TODO: Not sure code_point should support these operations
-	p_bool op_less_than(p_code_point other) const { return this->value < other.value; }
-	p_bool op_less_than_or_equal(p_code_point other) const { return this->value <= other.value; }
-	p_bool op_greater_than(p_code_point other) const { return this->value > other.value; }
-	p_bool op_greater_than_or_equal(p_code_point other) const { return this->value >= other.value; }
+	p_bool op_less_than(p_code_point other) const { return p_bool(this->raw_value < other.raw_value); }
+	p_bool op_less_than_or_equal(p_code_point other) const { return p_bool(this->raw_value <= other.raw_value); }
+	p_bool op_greater_than(p_code_point other) const { return p_bool(this->raw_value > other.raw_value); }
+	p_bool op_greater_than_or_equal(p_code_point other) const { return p_bool(this->raw_value >= other.raw_value); }
 
 };
 
@@ -541,15 +268,89 @@ public:
 	p_int LastIndexOf_(p_code_point c) const;
 	p_int index_of_(p_code_point c) const;
 
-	p_code_point op_Element(p_int const index) const { return Buffer[index.value]; }
+    // TODO check index bounds
+	p_code_point op_Element(p_int const index) const { return p_code_point(Buffer[index.value]); }
 	p_string op_add(p_string const & value) const;
-	p_bool op_equal(p_string const & other) const;
-	p_bool op_not_equal(p_string const & other) const { return !this->op_equal(other).value; }
 	p_bool op_less_than(p_string other) const;
 	p_bool op_less_than_or_equal(p_string other) const;
 	p_bool op_greater_than(p_string other) const;
 	p_bool op_greater_than_or_equal(p_string other) const;
 };
+
+// -----------------------------------------------------------------------------
+// Operators
+// -----------------------------------------------------------------------------
+
+inline auto equal_op(p_bool lhs, p_bool rhs) -> p_bool
+{
+    return p_bool(lhs.value == rhs.value);
+}
+inline auto equal_op(p_optional<p_bool> lhs, p_optional<p_bool> rhs) -> p_bool
+{
+    if(lhs.has_value().value)
+        return p_bool(rhs.has_value().value and equal_op(lhs.value(), rhs.value()).value);
+    else
+        return p_bool(not rhs.has_value().value);
+}
+
+inline auto equal_op(p_int lhs, p_int rhs) -> p_bool
+{
+    return p_bool(lhs.value == rhs.value);
+}
+inline auto equal_op(p_optional<p_int> lhs, p_optional<p_int> rhs) -> p_bool
+{
+    if(lhs.has_value().value)
+        return p_bool(rhs.has_value().value and equal_op(lhs.value(), rhs.value()).value);
+    else
+        return p_bool(not rhs.has_value().value);
+}
+
+inline auto equal_op(p_code_point lhs, p_code_point rhs) -> p_bool
+{
+    return p_bool(lhs.value() == rhs.value());
+}
+
+auto equal_op(p_string lhs, p_string rhs) -> p_bool;
+
+// TODO implement this without templates
+template<typename T>
+inline auto equal_op(T const *_Nullable lhs, None const & rhs) -> p_bool
+{
+    return p_bool(lhs == 0);
+}
+
+// TODO Get rid of this ability
+template<typename T>
+inline auto equal_op(T const *_Nullable lhs, T const *_Nullable const & rhs) -> p_bool
+{
+    return p_bool(lhs == 0);
+}
+
+inline auto not_equal_op(p_int lhs, p_int rhs) -> p_bool
+{
+    return p_bool(lhs.value != rhs.value);
+}
+inline auto not_equal_op(p_optional<p_int> lhs, p_optional<p_int> rhs) -> p_bool
+{
+    if(lhs.has_value().value)
+        return p_bool(not rhs.has_value().value or not_equal_op(lhs.value(), rhs.value()).value);
+    else
+        return p_bool(rhs.has_value().value);
+}
+
+// TODO implement this without templates
+template<typename T>
+inline auto not_equal_op(T lhs, T  rhs) -> p_bool
+{
+    return p_bool(not equal_op(lhs, rhs).value);
+}
+
+// TODO implement this without templates
+template<typename T>
+inline auto not_equal_op(T const *_Nullable lhs, None const & rhs) -> p_bool
+{
+    return p_bool(lhs != 0);
+}
 
 // -----------------------------------------------------------------------------
 // Standard Library
@@ -625,9 +426,6 @@ namespace system_
 			const_iterator end() const { return &values[length]; }
 
 			// Adamant Members
-			p_bool op_equal(List_<T> const *_Nonnull other) const { return this == other; }
-			p_bool op_not_equal(List_<T> const *_Nonnull other) const { return this != other; }
-
 			List_ *_Nonnull construct() { values = 0; length = 0; capacity = 0; return this; }
 			void Add_(T value) { add_(value); }
 			void Clear_() { clear_(); }
@@ -737,13 +535,10 @@ namespace system_
 			String_Builder_(): buffer(0), capacity(0), length(0) { }
 
 			// Adamant Members
-			p_bool op_equal(String_Builder_ const *_Nonnull other) const { return this == other; }
-			p_bool op_not_equal(String_Builder_ const *_Nonnull other) const { return this != other; }
-
 			String_Builder_ *_Nonnull construct() { return this; }
 			String_Builder_ *_Nonnull construct(p_string const & value);
 			String_Builder_ *_Nonnull construct_with_capacity(p_int capacity);
-            // TODO ByteLength should be a property
+            // TODO byte_length_ should be a property
 	        p_int byte_length_() const { return length; }
 			void Append_(p_string const & value);
 			void Append_(String_Builder_ const *_Nonnull value);
