c720001c9400d0c05d275992914e1c82c8c51117
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-05-17 16:33:14 -0400

    overload assert function to make message optional

---
+++ test-cases/contracts/assertNoMessage.cpp
@@ -0,0 +1,35 @@
+#include "RuntimeLibrary.hpp"
+
+// Type Declarations
+
+// Function Declarations
+void main__0();
+
+// Class Declarations
+
+// Global Definitions
+
+// Definitions
+
+void main__0()
+{
+	int__00 const x__ = int__00(5);
+	int__00 const y__ = int__00(5);
+	assert__1(not_equal_op(x__, y__));
+}
+
+// Entry Point Adapter
+std::int32_t main(int argc, char const *const * argv)
+{
+	try
+	{
+		main__0();
+		return 0;
+	}
+	catch(std::exception &ex)
+	{
+		std::printf("Program exited due to error:\n");
+		std::printf("%s", ex.what());
+		return 70;
+	}
+}
