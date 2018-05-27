7a6be81f23634cf28e88faab70b1a8df6c38195c
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-05-27 08:55:36 -0400

    fix test case broken by removal of implicit int to string conversion

--- test-cases/contracts/assert.cpp
+++ test-cases/contracts/assert.cpp
@@ -16,8 +16,8 @@ void main__0()
 	int32 const x__ = ((int32){5});
 	int32 const y__ = ((int32){5});
 	int32 const z__ = ((int32){6});
-	assert__2(equal_op(x__, y__), op__add(op__add(op__add(((string){2,(uint8_t const*)"x="}), x__), ((string){3,(uint8_t const*)" y="})), y__));
-	assert__2(equal_op(y__, z__), op__add(op__add(op__add(((string){2,(uint8_t const*)"y="}), y__), ((string){3,(uint8_t const*)" z="})), z__));
+	assert__2(equal_op(x__, y__), op__add(op__add(op__add(((string){2,(uint8_t const*)"x="}), int_to_string__1(x__)), ((string){3,(uint8_t const*)" y="})), int_to_string__1(y__)));
+	assert__2(equal_op(y__, z__), op__add(op__add(op__add(((string){2,(uint8_t const*)"y="}), int_to_string__1(y__)), ((string){3,(uint8_t const*)" z="})), int_to_string__1(z__)));
 }
 
 // Entry Point Adapter
