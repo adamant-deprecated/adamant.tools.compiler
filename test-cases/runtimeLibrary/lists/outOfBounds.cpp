34699147e067b2baee3625ff27ad99d67a5e2848
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-05-31 18:56:25 -0400

    use `Ints` class

--- test-cases/runtimeLibrary/lists/outOfBounds.cpp
+++ test-cases/runtimeLibrary/lists/outOfBounds.cpp
@@ -1,24 +1,33 @@
 #include "RuntimeLibrary.hpp"
 
 // Type Declarations
+typedef struct Test__0 Test__0;
 
 // Function Declarations
-int32 main__0();
+Test__0 *_Nonnull Test__0__0new__0(Test__0 *_Nonnull self);
+void main__0();
 
 // Class Declarations
+
+struct Test__0
+{
+};
 
 // Global Definitions
 
 // Definitions
 
-int32 main__0()
+Test__0 *_Nonnull Test__0__0new__0(Test__0 *_Nonnull self) { return self; }
+
+void main__0()
 {
-	system__collections__List__1<int32> const *_Nonnull const values__ = system__collections__List__1__0new__0<int32>(allocate(sizeof(system__collections__List__1<int32>)));
-	return system__collections__List__1__0op__element(values__, ((int32){0}));
+	system__collections__List__1<Test__0 const *_Nonnull> const *_Nonnull const values__ = system__collections__List__1__0new__0<Test__0 const *_Nonnull>(allocate(sizeof(system__collections__List__1<Test__0 const *_Nonnull>)));
+	system__collections__List__1__0op__element(values__, ((int32){0}));
 }
 
 // Entry Point Adapter
 int32_t main(int argc, char const *const * argv)
 {
-	return main__0().value;
+	main__0();
+	return 0;
 }
--- test-cases/runtimeLibrary/lists/referenceEquality.cpp
+++ test-cases/runtimeLibrary/lists/referenceEquality.cpp
@@ -1,19 +1,27 @@
 #include "RuntimeLibrary.hpp"
 
 // Type Declarations
+typedef struct Test__0 Test__0;
 
 // Function Declarations
+Test__0 *_Nonnull Test__0__0new__0(Test__0 *_Nonnull self);
 void main__0();
 
 // Class Declarations
+
+struct Test__0
+{
+};
 
 // Global Definitions
 
 // Definitions
 
+Test__0 *_Nonnull Test__0__0new__0(Test__0 *_Nonnull self) { return self; }
+
 void main__0()
 {
-	system__collections__List__1<int32> const *_Nonnull const values__ = system__collections__List__1__0new__0<int32>(allocate(sizeof(system__collections__List__1<int32>)));
+	system__collections__List__1<Test__0 const *_Nonnull> const *_Nonnull const values__ = system__collections__List__1__0new__0<Test__0 const *_Nonnull>(allocate(sizeof(system__collections__List__1<Test__0 const *_Nonnull>)));
 	void_ptr__0op__equal(values__, values__);
 	void_ptr__0op__not_equal(values__, values__);
 }
