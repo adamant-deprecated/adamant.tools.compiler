c64ab0fc1fc74a8cf0372a75ee2c22cc8c59655f
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-06-06 12:23:10 -0400

    Fix naming convetions in test

--- test-cases/classes/mutability.c
+++ test-cases/classes/mutability.c
@@ -2,11 +2,11 @@
 
 // Type Declarations
 typedef struct Mutable_Object__0 Mutable_Object__0;
-typedef struct immutable_object__0 immutable_object__0;
+typedef struct Immutable_object__0 Immutable_object__0;
 
 // Function Declarations
 Mutable_Object__0 *_Nonnull Mutable_Object__0__0new__0(Mutable_Object__0 *_Nonnull self);
-immutable_object__0 *_Nonnull immutable_object__0__0new__0(immutable_object__0 *_Nonnull self);
+Immutable_object__0 *_Nonnull Immutable_object__0__0new__0(Immutable_object__0 *_Nonnull self);
 void main__0();
 
 // Class Declarations
@@ -15,7 +15,7 @@ struct Mutable_Object__0
 {
 };
 
-struct immutable_object__0
+struct Immutable_object__0
 {
 };
 
@@ -25,12 +25,12 @@ struct immutable_object__0
 
 Mutable_Object__0 *_Nonnull Mutable_Object__0__0new__0(Mutable_Object__0 *_Nonnull self) { return self; }
 
-immutable_object__0 *_Nonnull immutable_object__0__0new__0(immutable_object__0 *_Nonnull self) { return self; }
+Immutable_object__0 *_Nonnull Immutable_object__0__0new__0(Immutable_object__0 *_Nonnull self) { return self; }
 
 void main__0()
 {
 	Mutable_Object__0 const *_Nonnull const m__ = Mutable_Object__0__0new__0(allocate(sizeof(Mutable_Object__0)));
-	immutable_object__0 const *_Nonnull const i__ = immutable_object__0__0new__0(allocate(sizeof(immutable_object__0)));
+	Immutable_object__0 const *_Nonnull const i__ = Immutable_object__0__0new__0(allocate(sizeof(Immutable_object__0)));
 }
 
 // Entry Point Adapter
