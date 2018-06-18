e4227953a55fd4155adaed502c43cee406232786
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-06-18 16:50:46 -0400

    start fixing null compatibility in runtime lib

--- test-cases/strings/concatenation.c
+++ test-cases/strings/concatenation.c
@@ -10,7 +10,7 @@ typedef enum Type_ID Type_ID;
 // Type Declarations
 
 // Function Declarations
-void mut main__0();
+void mut main__1(system__console__Console__0 mut ref const console__);
 
 // Class Declarations
 
@@ -18,14 +18,15 @@ void mut main__0();
 
 // Definitions
 
-void mut main__0()
+void mut main__1(system__console__Console__0 mut ref const console__)
 {
 	string mut x__ = string__0op__add(string__0op__add(string__0op__add(((string){{6},(uint8_t*)u8"Hello "}), ((string){{7},(uint8_t*)u8"World! "})), int_to_string__1(((int32){4}))), code_point_to_string__1(((code_point){/*c*/0x63})));
+	console_write_line__2(console__, x__);
 }
 
 // Entry Point Adapter
 int32_t main(int argc, char const ptr const ptr argv)
 {
-	main__0();
+	main__1(allocate(sizeof(system__console__Console__0)));
 	return 0;
 }
