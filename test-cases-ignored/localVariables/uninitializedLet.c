a7e1218bcfbf36f7bd82923f481a73064d897915
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-06-21 08:40:27 -0400

    test cases from another branch for uninitalized let and var (ignored)

---
+++ test-cases-ignored/localVariables/uninitializedLet.c
@@ -0,0 +1,31 @@
+#include "RuntimeLibrary.h"
+
+// Type ID Declarations
+enum Type_ID
+{
+	never__0Type__ID = 0,
+};
+typedef enum Type_ID Type_ID;
+
+// Type Declarations
+
+// Function Declarations
+int32 mut Main__0();
+
+// Class Declarations
+
+// Global Definitions
+
+// Definitions
+
+int32 mut Main__0()
+{
+	int32 const result__;
+	return result__;
+}
+
+// Entry Point Adapter
+int32_t main(int argc, char const ptr const ptr argv)
+{
+	return ().value;
+}
