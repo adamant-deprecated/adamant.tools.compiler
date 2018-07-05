33181caf260db6d75cae6643e3dcabf0d0df6c60
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-07-05 19:41:48 -0400

    support optional never type

---
+++ test-cases/never/optionalNever.c
@@ -0,0 +1,38 @@
+#include "RuntimeLibrary.h"
+
+// Type ID Declarations
+enum Type_ID
+{
+	never__0Type__ID = 0,
+};
+typedef enum Type_ID Type_ID;
+
+// Type Declarations
+
+// Function Declarations
+int32 mut main__0();
+
+// Class Declarations
+
+// Global Definitions
+
+// Definitions
+
+int32 mut main__0()
+{
+	optional__never mut x__ = none;
+	if (cond(never__0op__equal(x__, none)))
+	{
+		return ((int32){0});
+	}
+	else
+	{
+		return ((int32){1});
+	}
+}
+
+// Entry Point Adapter
+int32_t main(int argc, char const ptr const ptr argv)
+{
+	return main__0().value;
+}
